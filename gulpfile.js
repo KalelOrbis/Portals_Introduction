c4a8b1704bdfe650c87e5c5b73851b98c7cf55d8
KalelOrbis <116343195+KalelOrbis@users.noreply.github.com>
2022-10-28 15:57:12 +0200

    Completed lesson 20 purge css

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss')
 
 function buildStyles() {
     // shinigami/**/ means any subfolder inside the shinigami folder 
   return src('shinigami/**/*.scss')
     .pipe(sass())
+    .pipe(purgecss({ content: ['*.html']}))
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['shinigami/**/*.scss'], buildStyles)
+  watch(['shinigami/**/*.scss','*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
